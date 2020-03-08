1a2faa004ef3d779c776df7a22809a58db1c8041
WitoldSynowiec <witold.synowiec.stud@pw.edu.pl>
2020-03-08 14:08:03 +0100

    Poprawka błędu związanego  ze złym  grupowaniem

--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -2,94 +2,87 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.UIManager.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-//klasa mająca odpowiadać za główne  okno
-public class MainWindow extends JFrame {
-    static JButton b, b1, b2, b3;
-
-
-    public MainWindow() {
-        this.setTitle("Rejestracja wyników badań");
-        this.setSize(1024, 768);
-//        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+public class MainWindow extends JFrame implements ActionListener {
+    public static LeftPanel leftPanel;
+    public static  RightPanel  rightPanel;
+    public MainWindow(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.setResizable(false);
+        this.setResizable(true);
+        this.setSize(1024, 768);
 
+        leftPanel = new LeftPanel(this);
+        rightPanel = new RightPanel(this);
 
-        // panel odpowiedzialny za lewą strone, ma sie tu znaleźć blok Dane Pacjenta i Badanie
-        JPanel lPanel = new JPanel(new BorderLayout());
-        lPanel.setPreferredSize(new Dimension(getWidth() * 2 / 5, getHeight()));
-        lPanel.setBackground(Color.lightGray);
-        this.add(lPanel, BorderLayout.WEST);
+        this.setLayout(new BorderLayout());
+        this.add(BorderLayout.EAST,rightPanel);
+        this.add(BorderLayout.WEST,leftPanel);
 
-
-        //panel Dane Pacjenta
-        JPanel pdPanel = new JPanel();
-        pdPanel.setBackground(Color.lightGray);
-        pdPanel.setBorder(BorderFactory.createTitledBorder("Dane Pacjenta"));
-//        pdPanel.setPreferredSize(new Dimension(getWidth(), this.getContentPane().getHeight() * 3 / 5));
-        pdPanel.setBounds(30, 30, 40, 40);
-
-        lPanel.add(pdPanel, BorderLayout.NORTH);
-
-        JLabel getNameLabel = new JLabel("Imię: ");
-        JTextField getNameTextField = new JTextField(10);
-        JLabel getSurnameLabel = new JLabel("Nazwisko: ");
-        JTextField getSurnameTextField = new JTextField(10);
-        JLabel getPeselLabel = new JLabel("Pesel: ");
-        JTextField getPeselTextField = new JTextField(10);
-
-        JLabel getSexLabel = new JLabel("Płeć: ");
-        JRadioButton getWomanRadioButton = new JRadioButton("Kobieta", false);
-        JRadioButton getManRadioButton = new JRadioButton("Mężczyzna", false);
-//        this.add(getWomanRadioButton);
-//        this.add(getManRadioButton);
-
-        JLabel getInsLabel = new JLabel("Ubezpieczenie");
-        String[] ins_states = {"Prywatne", "Brak"};
-        JComboBox<String> iComboBox = new JComboBox<>(ins_states);
-//        iComboBox.setEditable(false);
-
-        JButton spdButton = new JButton("Zapisz");
-        JButton cpdButton = new JButton("Anuluj");
-        spdButton.setMargin(new Insets(0,0,0,0));
-        cpdButton.setMargin(new Insets(0,0,0,0));
-        spdButton.setAlignmentX(LEFT_ALIGNMENT);
-        cpdButton.setAlignmentX(LEFT_ALIGNMENT);
-        GroupLayout patientDataLayout = new GroupLayout(pdPanel);
-        pdPanel.setLayout(patientDataLayout);
-        patientDataLayout.setAutoCreateGaps(true);
-
-        //grupowanie po pionie
-        patientDataLayout.setHorizontalGroup(patientDataLayout.createSequentialGroup().addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(getNameLabel).addComponent(getSurnameLabel).addComponent(getPeselLabel).addComponent(getSexLabel).addComponent(getInsLabel).addGroup(patientDataLayout.createSequentialGroup().addComponent(spdButton).addComponent(cpdButton))).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(getNameTextField).addComponent(getSurnameTextField).addComponent(getPeselTextField).addGroup(patientDataLayout.createSequentialGroup().addComponent(getWomanRadioButton).addComponent(getManRadioButton)).addComponent(iComboBox)));
-        //grupowanie po poziomie
-        patientDataLayout.setVerticalGroup(patientDataLayout.createSequentialGroup().addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getNameLabel).addComponent(getNameTextField)).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getSurnameLabel).addComponent(getSurnameTextField)).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getPeselLabel).addComponent(getPeselTextField)).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getSexLabel).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getWomanRadioButton).addComponent(getManRadioButton))).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(getInsLabel).addComponent(iComboBox)).addGroup(patientDataLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(spdButton).addComponent(cpdButton)));
-
-        ///DO OPRACOWANIA!
-        //panel Badania
-        JPanel examinationPanel = new JPanel(new BorderLayout());
-        examinationPanel.setBackground(Color.lightGray);
-        examinationPanel.setBorder(BorderFactory.createTitledBorder("Badanie"));
-//        examinationPanel.setPreferredSize(new Dimension(getWidth(), this.getContentPane().getHeight() * 2 / 5));
-        examinationPanel.setBounds(30, 30, 40, 40);
-        b2 = new JButton("buttonx");
-        b3 = new JButton("buttony");
-        examinationPanel.add(b2, BorderLayout.NORTH);
-//        examinationPanel.add(b3, BorderLayout.);
-        lPanel.add(examinationPanel, BorderLayout.CENTER);
-
-
-        //panel prawy
-        JPanel rPanel = new JPanel(new BorderLayout());
-        rPanel.setPreferredSize(new Dimension(getWidth() * 3 / 5, getHeight()));
-        rPanel.setBorder(BorderFactory.createTitledBorder("Lista Pacjentów: "));
-        rPanel.setBackground(Color.lightGray);
-        this.add(rPanel, BorderLayout.EAST);
-        JTable plTable = new JTable();
-        rPanel.add(plTable);
-
+        leftPanel.pdPanel.spdButton.addActionListener(this);
+        leftPanel.pdPanel.cpdButton.addActionListener(this);
+        leftPanel.ePanel.button1.addActionListener(this);
+        leftPanel.ePanel.button2.addActionListener(this);
+        leftPanel.ePanel.button3.addActionListener(this);
+        leftPanel.pdPanel.getManRadioButton.addActionListener(this);
+        leftPanel.pdPanel.getWomanRadioButton.addActionListener(this);
     }
 
+
+    private void clear_pd_sheet() {
+        leftPanel.pdPanel.getNameTextField.setText("");
+        leftPanel.pdPanel.getSurnameTextField.setText("");
+        leftPanel.pdPanel.getPeselTextField.setText("");
+        leftPanel.pdPanel.getWomanRadioButton.setSelected(false);
+        leftPanel.pdPanel.getManRadioButton.setSelected(false);
+        leftPanel.pdPanel.iComboBox.setSelectedIndex(0);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        // tylko jedna płeć może być zaznaczona
+        if (source == leftPanel.pdPanel.getManRadioButton && leftPanel.pdPanel.getWomanRadioButton.isSelected())
+            leftPanel.pdPanel.getWomanRadioButton.setSelected(false);
+        else if  (source == leftPanel.pdPanel.getWomanRadioButton && leftPanel.pdPanel.getManRadioButton.isSelected())
+            leftPanel.pdPanel.getManRadioButton.setSelected(false);
+
+        //
+        if (source == leftPanel.pdPanel.spdButton)
+        {
+            PatientRecord.Sex_enum sex;
+            if (leftPanel.pdPanel.getManRadioButton.isSelected())
+                sex = PatientRecord.Sex_enum.M;
+            else
+                sex = PatientRecord.Sex_enum.K;
+
+            PatientRecord.Insurance_enum ins;
+            if (leftPanel.pdPanel.iComboBox.getSelectedIndex()==1)
+                ins =  PatientRecord.Insurance_enum.LACK;
+            else if (leftPanel.pdPanel.iComboBox.getSelectedIndex()==2)
+                ins =  PatientRecord.Insurance_enum.NFZ;
+            else if (leftPanel.pdPanel.iComboBox.getSelectedIndex()==3)
+                ins =  PatientRecord.Insurance_enum.PRIVATE;
+            else
+                ins =  PatientRecord.Insurance_enum.NO_ANS;
+
+            //tworzenie nowego obiektu listy patient_database
+            PatientRegister.patient_database.add(new PatientRecord(leftPanel.pdPanel.getNameTextField.getText(),leftPanel.pdPanel.getSurnameTextField.getText(),Long.parseLong(leftPanel.pdPanel.getPeselTextField.getText()), sex, ins, false));
+            clear_pd_sheet();
+
+        }
+        //jeśli klikamy anuluj resetujemy pola
+        else if (source == leftPanel.pdPanel.cpdButton)
+        {
+            clear_pd_sheet();
+        }
+
+//        if ((source == leftPanel.pdPanel.spdButton || source == leftPanel.pdPanel.cpdButton)&&(leftPanel.pdPanel.getNameTextField.getText().isEmpty()||leftPanel.pdPanel.getSurnameTextField.getText().isEmpty()||leftPanel.pdPanel.getPeselTextField.getText().isEmpty()))  return;
+////        if (source == leftPanel.pdPanel.getManRadioButton && )
+
+
+    }
 }
